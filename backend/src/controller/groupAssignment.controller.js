a0da0ccbe3fd403fc61d454138fa96f00e1d5ccd
raurey <roger.guzman@mercadolibre.com>
2024-04-17 01:55:41 -0500

    Assignment services

--- backend/src/controller/groupAssignment.controller.js
+++ backend/src/controller/groupAssignment.controller.js
@@ -3,8 +3,6 @@ import assignmentModel from '../models/groupAssignment.model.js'
 export const User2group = async (req, res) => {
     const Request = req.body;
 
-    
-
     if (!Request.user_id) {
         return res.status(400).json({ error_message: 'user_id is required' });
     }
@@ -13,13 +11,35 @@ export const User2group = async (req, res) => {
     }
 
     const groupAssigment = new assignmentModel({
-       
+
         group_id: Request.group_id,
         user_id: Request.user_id,
-        
+
 
     })
     const createdGroup = await groupAssigment.save()
     return res.status(200).json({ id: createdGroup._id });
 
 };
+
+
+
+
+export const findAssignmentByGroup = async (req, res) => {
+
+    const group_id = req.params.group_id
+
+    const aaa = await assignmentModel.find({ group_id: group_id },{user_name:1})
+        .populate('user_id', { username: 1 })
+        .then(assignment => {
+            return res.status(200).json(assignment)
+        })
+        .catch(error => {
+            console.error(error);
+            res.send("error")
+            return res.status(500).json({ error_message: 'group_id is required' });
+        });
+
+};
+
+
---
+++ react-frontend/src/components/maps/MapHandlerSelect.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { APIProvider, Map, Marker, InfoWindow } from '@vis.gl/react-google-maps';
+import { Box } from '@mui/material';
+
+
+const MapHandlerSelect = ({}) => {
+
+  const [infowindowOpen, setInfowindowOpen] = useState(false);
+
+  const [pos, setPos] = useState({});
+
+
+  //const pos = { lat: 10, lng: -4 };
+
+  return (
+     <Box  sx={{
+        height: '100vh',
+        width: '100%', 
+      }}>
+      <APIProvider apiKey={'AIzaSyBhJ6Mz2-NBrPZeuXtlaROqHrT2hN0aKVg'}>
+        <Map
+          defaultZoom={10}
+          defaultCenter={{ lat:40.5486807849397, lng:-111.9137212403442 }}
+          gestureHandling={'greedy'}
+          style={{ width: '100%', height: '100%' }} 
+        >
+
+          {data.map((location, index) => (
+            <Marker
+              position={location}
+              clickable={true}
+              title={'clickable google.maps.Marker'}
+            />
+          ))}
+
+        </Map>
+
+      </APIProvider>
+      </Box>
+  
+
+  );
+}
+
+export default MapHandlerSelect;
+
+
