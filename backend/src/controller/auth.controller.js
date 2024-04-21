981ecd3b2312155b24feef81aac3acffdb731ff9
raurey <00.raul.11@gmail.com>
2024-04-20 19:46:20 -0600

    return user name after login

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -7,31 +7,26 @@ dotenv.config()
 const jwtSecret = process.env.JWT_SECRET;
 
 
-
-
-
 const saltRounds = 10;
 
-export const signin = async (req, res) =>{
-    
-        try {
-            const { username, password } = req.body;
-            const user = await userModel.findOne({ username });
-            if (!user) {
-                return res.status(404).json({ success: false, message: 'User not found' });
-            }
-            const isMatch = await bcrypt.compare(password, user.password);
-            if (!isMatch) {
-                return res.status(401).json({ success: false, message: 'Incorrect password' });
-            }
-            const token = jwt.sign({ id: user._id, username: user.username },jwtSecret, { expiresIn: '1h' });
-            res.json({ success: true, token });
-        } catch (error) {
-            console.error('Error authenticating user:', error);
-            res.status(500).json({ success: false, message: 'Error authenticating user' });
+export const signin = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await userModel.findOne({ username });
+        if (user && await bcrypt.compare(password, user.password)) {
+            const token = jwt.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '24h' });
+            res.json({
+                message: 'Authentication successful!',
+                token,
+                username: user.username // Include the username in the response
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid credentials' });
         }
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error });
+    }
 };
-
 
 export const signup = async (req, res) =>{
     try {
